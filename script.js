961698c5dbaa946db918bc0b64c1ded90b118a8a
Espi Nita <lucascolegioripari@gmail.com>
2021-12-07 23:14:46 -0300

    Added a return statement to the functions so that i can actually use them)

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function computerPlay(arr) {
   let playComp = arr[Math.floor(Math.random() * arr.length)];
 
-  console.log(playComp);
+  return(playComp)
 }
-computerPlay(["Rock" , "Paper" , "Scissors"]);
+let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"]);
+console.log(comPlay);
 
 function playerSelection(choice) {
   let lowerCaseSelection = choice.toLowerCase(); 
   let capitalizedSelection = lowerCaseSelection.charAt(0).toUpperCase()+lowerCaseSelection.slice(1);
   let play = `You picked ${capitalizedSelection}`;
  
- console.log(play)
+ return(play)
 }
- playerSelection(prompt("Rock/Paper/Scissors",))
-
-
+ let playerPlay = playerSelection(prompt("Rock/Paper/Scissors",));
+ console.log(playerPlay)
