5c4fd6eaba5fdce470e32845e105266bcec57120
Espi Nita <lucascolegioripari@gmail.com>
2021-12-10 00:29:36 -0300

    Fixed the return values, and implemented the rules of the game with conditionals.

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 function computerPlay(arr) {
   let playComp = arr[Math.floor(Math.random() * arr.length)];
-
-  return(playComp)
+  let playCompMessage = `Computer picked ${playComp}`;
+  
+  console.log(playCompMessage);
+  return(playComp);
 }
 let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"]);
-console.log(comPlay);
+
 
 function playerSelection(choice) {
   let lowerCaseSelection = choice.toLowerCase(); 
   let capitalizedSelection = lowerCaseSelection.charAt(0).toUpperCase()+lowerCaseSelection.slice(1);
   let play = `You picked ${capitalizedSelection}`;
  
- return(play)
+ console.log(play)
+ return(capitalizedSelection)
+ 
 }
  let playerPlay = playerSelection(prompt("Rock/Paper/Scissors",));
- console.log(playerPlay)
+ 
+if (playerPlay === "Rock"){
+  
+  if(comPlay ==="Rock"){
+    console.log("Tie")
+  }
+   else if(comPlay === "Paper") {
+     console.log("Computer Wins")
+   }
+
+   else if(comPlay === "Scissors"){
+     console.log("Player Wins")
+   }
+  
+}
+
+else if(playerPlay === "Paper"){
+    
+    if(comPlay === "Paper"){
+      console.log("Tie")
+    }
+
+    else if(comPlay === "Rock"){
+      console.log("Player Wins")
+    }
+
+    else if(comPlay === "Scissors"){
+      console.log("Computer Wins")
+    }
+
+}
+ 
+else if(playerPlay === "Scissors"){
+    
+    if(comPlay === "Scissors"){
+      console.log("Tie")
+    }
+
+    else if(comPlay === "Paper"){
+      console.log("Player wins")
+    }
+
+    else if(comPlay === "Rock"){
+      console.log("Computer Wins")
+    }
+}
