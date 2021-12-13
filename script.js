a7cdaa1da3428737254d6dc7a4fa384c4d4c9339
Espi Nita <lucascolegioripari@gmail.com>
2021-12-13 03:19:05 -0300

    Added a main function to loop so we can play rounds, struggling with the scoreboard, but it looks alright so far

--- script.js
+++ script.js
@@ -1,66 +1,126 @@
-function computerPlay(arr) {
-  let playComp = arr[Math.floor(Math.random() * arr.length)];
-  let playCompMessage = `Computer picked ${playComp}`;
-  
-  console.log(playCompMessage);
-  return(playComp);
-}
-let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"]);
+let playerCount = 0;
+let compCount = 0;
 
-
-function playerSelection(choice) {
+function fullRound(){
+ function playerSelection(choice){
   let lowerCaseSelection = choice.toLowerCase(); 
   let capitalizedSelection = lowerCaseSelection.charAt(0).toUpperCase()+lowerCaseSelection.slice(1);
   let play = `You picked ${capitalizedSelection}`;
  
  console.log(play)
  return(capitalizedSelection)
- 
 }
- let playerPlay = playerSelection(prompt("Rock/Paper/Scissors",));
+
+let playerPlay = playerSelection(prompt("Rock/Paper/Scissors"));
+
  
-if (playerPlay === "Rock"){
+  function computerPlay(arr) {
+  let playComp = arr[Math.floor(Math.random() * arr.length)];
+  let playCompMessage = `Computer picked ${playComp}`;
   
-  if(comPlay ==="Rock"){
-    console.log("Tie")
+  console.log(playCompMessage);
+  return(playComp);
+}
+let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"])
+ 
+ 
+function game(a,b){
+
+  
+  if (a === "Rock"){
+  
+  if(b ==="Rock"){
+    console.log("Tie");
+    return(playerCount,compCount)
+    
   }
-   else if(comPlay === "Paper") {
-     console.log("Computer Wins")
+   else if(b === "Paper") {
+     console.log("Computer Wins");
+     console.log(playerCount , compCount + 1);
+     return( compCount + 1)
    }
 
-   else if(comPlay === "Scissors"){
-     console.log("Player Wins")
+   else if(b === "Scissors"){
+     console.log("Player Wins");
+     console.log(playerCount + 1 , compCount);
+     return(playerCount + 1)
    }
   
 }
 
-else if(playerPlay === "Paper"){
+else if(a === "Paper"){
     
-    if(comPlay === "Paper"){
-      console.log("Tie")
+    if(b === "Paper"){
+      console.log("Tie");
+      return(playerCount,compCount)
     }
 
-    else if(comPlay === "Rock"){
-      console.log("Player Wins")
+    else if(b === "Rock"){
+      console.log("Player Wins");
+      console.log(playerCount + 1 , compCount);
+      return(playerCount + 1)
     }
 
-    else if(comPlay === "Scissors"){
-      console.log("Computer Wins")
+    else if(b === "Scissors"){
+      console.log("Computer Wins");
+      console.log(playerCount , compCount+1);  
+      return(compCount + 1)
     }
 
 }
  
-else if(playerPlay === "Scissors"){
+else if(a === "Scissors"){
     
-    if(comPlay === "Scissors"){
-      console.log("Tie")
+    if(b === "Scissors"){
+      console.log("Tie");
+      return(playerCount,compCount)
     }
 
-    else if(comPlay === "Paper"){
-      console.log("Player wins")
+    else if(b === "Paper"){
+      console.log("Player Wins");
+      console.log(playerCount + 1 , compCount);
+      return(playerCount + 1)
     }
 
-    else if(comPlay === "Rock"){
-      console.log("Computer Wins")
-    }
+    else if(b === "Rock"){
+      console.log("Computer Wins");
+      console.log(playerCount , compCount + 1);
+      return(compCount + 1)
+    }                                  
+    
+    
+  }
+  return(playerCount, compCount)
+  
+}game(playerPlay,comPlay)
+for(i= 0;i = 5; i++){
+  fullRound()
+
+  if(compCount >= 3){
+    console.log("Victory goes to the computer")
+  }
+  else if(  playerCount >= 3){
+    console.log("Epic win Player")
+  }
 }
+
+
+
+
+}fullRound()
+
+for(i= 0;i = 5; i++){
+  fullRound()
+
+  if(compCount >= 3){
+    console.log("Victory goes to the computer")
+  }
+  else if(  playerCount >= 3){
+    console.log("Epic win Player")
+  }
+}
+
+
+ 
+  
+
