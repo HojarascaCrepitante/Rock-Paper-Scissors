3cc9f462fd1c68b859e6a65fcd6ecf575b22e1ac
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-03-31 01:55:39 -0300

    Figured out how to make the counter work, onclick events run after the page is loaded so I will add the if else statements inside the play function

--- script.js
+++ script.js
@@ -1,114 +1,69 @@
+const results = document.querySelector('#results')
+const playerPoints = document.createElement('p')
+  playerPoints.textContent = '0';
+  playerPoints.setAttribute('id', 'player')
+  results.appendChild(playerPoints)
+const computerPoints = document.createElement('p')
+    computerPoints.textContent = '0';
+    computerPoints.setAttribute('id', 'computer')
+    results.appendChild(computerPoints)
 
 const container = document.querySelector('#container');
-const button = document.createElement('button');
-button.setAttribute('id', 'Rock');
-button.textContent = 'Rock';
-button.addEventListener('click', play)
-container.appendChild(button);
+  const rockButton = document.createElement('button');
+    rockButton.setAttribute('id', 'Rock');
+    rockButton.textContent = 'Rock';
+    rockButton.addEventListener('click', play)
+    container.appendChild(rockButton);
 
-const button2 = document.createElement('button');
-button2.setAttribute('id','Paper')
-button2.textContent = 'Paper';
-button2.addEventListener('click', play)
-container.appendChild(button2);
+const paperButton = document.createElement('button');
+  paperButton.setAttribute('id','Paper')
+  paperButton.textContent = 'Paper';
+  paperButton.addEventListener('click', play)
+  container.appendChild(paperButton);
 
-const button3 = document.createElement('button');
-button3.setAttribute('id', 'Scissors')
-button3.textContent = 'Scissors';
-button3.addEventListener('click', play)
-container.appendChild(button3);
+const scissorsButton = document.createElement('button');
+  scissorsButton.setAttribute('id', 'Scissors')
+  scissorsButton.textContent = 'Scissors';
+  scissorsButton.addEventListener('click', play)
+  container.appendChild(scissorsButton);
 
+  let playerChoice = '';
+  
   function play(e){
+    
     if (e.target.id == 'Rock'){
-      console.log(e.target.textContent)
+      console.log(e.target.textContent);
+      playerChoice = e.target.textContent;
+      if(comPlay == 'Paper'){
+        console.log(playerPoints.textContent++)
+      }
+
     }
     else if (e.target.id == 'Paper'){
-      console.log(e.target.textContent)
+      console.log(e.target.textContent);
+      playerChoice = e.target.textContent;
     }
-    else (e.target.id == 'Scissors')
-      console.log(e.target.textContent)
-    
+    else if (e.target.id == 'Scissors') {
+      console.log(e.target.textContent);
+      playerChoice = e.target.textContent;
+    }
+      
+      console.log(playerChoice)
+      return(playerChoice)
   }
-
-  function computerPlay(arr) {
+ 
+ 
+function computerPlay(arr) {
   let playComp = arr[Math.floor(Math.random() * arr.length)];
   let playCompMessage = `Computer picked ${playComp}`;
   
   console.log(playCompMessage);
   return(playComp);
 }
+
 let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"])
  
- 
-function game(a,b,c,d){
 
-  
-  if (a === "Rock"){
-  
-  if(b ==="Rock"){
-    console.log("Tie");
-    return(count1,count2)
-    
-  }
-   else if(b === "Paper") {
-     console.log("Computer Wins");
-     console.log(count1, count2 + 1);
-     return( count2 + 1)
-   }
-
-   else if(b === "Scissors"){
-     console.log("Player Wins");
-     console.log(count1 + 1 , count2);
-     return(count1 + 1)
-   }
-  
-}
-
-else if(c === "Paper"){
-    
-    if(b === "Paper"){
-      console.log("Tie");
-      return(count1,count2)
-    }
-
-    else if(b === "Rock"){
-      console.log("Player Wins");
-      console.log(count1 + 1 , count2);
-      return(count1 + 1)
-    }
-
-    else if(b === "Scissors"){
-      console.log("Computer Wins");
-      console.log(count1 , count2+1);  
-      return(count2 + 1)
-    }
-
-}
- 
-else if(d === "Scissors"){
-    
-    if(b === "Scissors"){
-      console.log("Tie");
-      return(count1,count2)
-    }
-
-    else if(b === "Paper"){
-      console.log("Player Wins");
-      console.log(count1 + 1 , count2);
-      return(count1 + 1)
-    }
-
-    else if(b === "Rock"){
-      console.log("Computer Wins");
-      console.log(count1 , count2 + 1);
-      return(count2 + 1)
-    }                                  
-    
-    
-  }
-  return(count1, count2)
-  
-}game(choice1,comPlay,choice2,choice3)
 
 
 
