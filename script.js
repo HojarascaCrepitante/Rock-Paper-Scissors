6b5b3a1436cae8d40ff3dfea8be617f61e12ba51
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-04-02 19:06:29 -0300

    I made the display of the moves so it would not display the history of moves, instead of the current move, also separated the functions and simply called them inside the play() function, now only some minor tweaks left

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
 const header = document.querySelector('.header');
-let round = document.querySelector('#round')
-  round.textContent = 'Round'
+let round = document.querySelector('#round');
+  round.textContent = 'Round';
   round.classList.add('score');
-  let representationOfRound = document.createElement('p')
-  representationOfRound.textContent = '0'
-  round.appendChild(representationOfRound)
-  header.appendChild(round)
-const resultsPlayer = document.querySelector('#resultsPlayer')
-  resultsPlayer.textContent = 'Player' 
+  let representationOfRound = document.createElement('p');
+  representationOfRound.textContent = '0';
+  round.appendChild(representationOfRound);
+  header.appendChild(round);
+const resultsPlayer = document.querySelector('#resultsPlayer');
+  resultsPlayer.textContent = 'Player' ;
   resultsPlayer.classList.add('score');
-  let playerPoints = document.createElement('p')
+  let playerPoints = document.createElement('p');
     playerPoints.textContent = '0';
     playerPoints.setAttribute('id', 'player');
-    resultsPlayer.appendChild(playerPoints)
-    header.insertBefore(resultsPlayer,round)
-const resultsComputer = document.querySelector('#resultsComputer')
+    resultsPlayer.appendChild(playerPoints);
+    header.insertBefore(resultsPlayer,round);
+const resultsComputer = document.querySelector('#resultsComputer');
   resultsComputer.textContent = 'Computer';
   resultsComputer.classList.add('score');
-  let computerPoints = document.createElement('p')
+  let computerPoints = document.createElement('p');
     computerPoints.textContent ='0';
-    computerPoints.setAttribute('id', 'computer')
-    resultsComputer.appendChild(computerPoints)
-    header.appendChild(resultsComputer)
+    computerPoints.setAttribute('id', 'computer');
+    resultsComputer.appendChild(computerPoints);
+    header.appendChild(resultsComputer);
 
 const everyButton = document.querySelector('#everyButton');
   const rockButton = document.createElement('button');
     rockButton.setAttribute('id', 'Rock');
     rockButton.textContent = 'Rock';
-    rockButton.addEventListener('click', play)
+    rockButton.addEventListener('click', play);
+    rockButton.addEventListener('click', clearPreviousSelection)
+    rockButton.addEventListener('click', appendSelections)
     everyButton.appendChild(rockButton);
 
     const paperButton = document.createElement('button');
-    paperButton.setAttribute('id','Paper')
+    paperButton.setAttribute('id','Paper');
     paperButton.textContent = 'Paper';
-    paperButton.addEventListener('click', play)
+    paperButton.addEventListener('click', play);
+    paperButton.addEventListener('click', clearPreviousSelection)
+    paperButton.addEventListener('click', appendSelections);
     everyButton.appendChild(paperButton);
 
     const scissorsButton = document.createElement('button');
-    scissorsButton.setAttribute('id', 'Scissors')
+    scissorsButton.setAttribute('id', 'Scissors');
     scissorsButton.textContent = 'Scissors';
-    scissorsButton.addEventListener('click', play)
+    scissorsButton.addEventListener('click', play);
+    scissorsButton.addEventListener('click', clearPreviousSelection)
+    scissorsButton.addEventListener('click', appendSelections);
     everyButton.appendChild(scissorsButton);
 
-  const playerAndComputerSelections = document.querySelector('.playerAndComputerSelections')
+  const playerAndComputerSelections = document.querySelector('.playerAndComputerSelections');
 
     
  
   function play(e){
-    function computerPlay(arr) {
-      let playComp = arr[Math.floor(Math.random() * arr.length)];
-      let playCompMessage = `Computer picked ${playComp}`;
-      console.log(playCompMessage);
-      let computerSelection = document.createElement('p')
-      computerSelection.textContent = `${playCompMessage}`
-      playerAndComputerSelections.appendChild(computerSelection)
-      return(playComp);
+
+    let checkIfComputerSelectionExists = document.getElementById('computerSelection');
+      if (checkIfComputerSelectionExists){
+        playerAndComputerSelections.removeChild(computerSelection);
       }
-      let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"])
+    
+    let comPlay = computerPlay(["Rock" , "Paper" , "Scissors"])
 
                
     if (e.target.id == 'Rock'){
@@ -106,11 +109,21 @@ const everyButton = document.querySelector('#everyButton');
         }
     }
       representationOfRound.textContent++
-        let playerSelection = document.createElement('p')
-        playerSelection.textContent = `You Picked ${e.target.id}`
-        playerAndComputerSelections.appendChild(playerSelection);
+        
     checkWinner()
     }
+    
+    function computerPlay(arr) {
+      let playComp = arr[Math.floor(Math.random() * arr.length)];
+      let playCompMessage = `Computer picked ${playComp}`;
+      console.log(playCompMessage);
+      let computerSelection = document.createElement('p')
+      computerSelection.setAttribute('id', 'computerSelection')
+      computerSelection.textContent = `${playCompMessage}`
+      playerAndComputerSelections.appendChild(computerSelection)
+      return(playComp);
+      }
+      
       
     function checkWinner(){
       if (representationOfRound.textContent == 5 && playerPoints.textContent > computerPoints.textContent){
@@ -126,7 +139,21 @@ const everyButton = document.querySelector('#everyButton');
         location.reload()
       }
      }
-    
+
+     
+     function appendSelections(e){
+        let playerSelection = document.createElement('p')
+        playerSelection.setAttribute('id','playerSelection')
+        playerSelection.textContent = `You Picked ${e.target.id}`
+        playerAndComputerSelections.appendChild(playerSelection);
+        return playerSelection
+      } 
+      function clearPreviousSelection(){
+         let checkIfPlayerSelectionExists = document.getElementById('playerSelection');
+          if (checkIfPlayerSelectionExists){
+          playerAndComputerSelections.removeChild(playerSelection);
+          }
+      }
 
 
   
